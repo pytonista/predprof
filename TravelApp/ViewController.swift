681bb1773eca4690c105af5bad72cbda65324d49
Shadowkiller213 <ruslanakuev312@gmail.com>
2022-02-03 18:59:18 +0300

    Initial Commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by Akuev Ruslan on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
